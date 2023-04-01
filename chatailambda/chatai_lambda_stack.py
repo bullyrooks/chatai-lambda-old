d43e5922835e8807f6580ebb0dfa52cee46f3705
brian <brian.rook@gmail.com>
2023-04-01 14:29:43 -0600

    fix: add api key to request

--- chatailambda/chatai_lambda_stack.py
+++ chatailambda/chatai_lambda_stack.py
@@ -1,10 +1,10 @@
+import os
+
 from aws_cdk import (
     # Duration,
     Stack,
-    aws_lambda as _lambda,
-    aws_apigateway as apigateway, CfnParameter,
-)
-import os
+    aws_lambda as _lambda )
+from aws_cdk.aws_apigateway import ApiKey, UsagePlan, RestApi, UsagePlanProps, ThrottleSettings, LambdaIntegration
 from aws_cdk.aws_ecr import Repository
 from constructs import Construct
 
@@ -25,19 +25,42 @@ class ChatAILambdaStack(Stack):
         self.chatai_lambda = _lambda.DockerImageFunction(
             scope=self,
             id="chatai-lambda",
-            # Function name on AWS
             function_name="chatai_message",
-            # Use aws_cdk.aws_lambda.DockerImageCode.from_image_asset to build
-            # a docker image on deployment
             code=self.ecr_image,
         )
 
-        api = apigateway.RestApi(self, "chatai-lambda-gw",
+        api = RestApi(self, "chatai-lambda-gw",
                                  rest_api_name="ChatAI Service",
-                                 description="This service fronts chatai.")
+                                 description="This service fronts chatai.",
+                                 deploy_options={
+                                     "api_key_source_type": "HEADER"
+                                 })
 
-        chatai_integration = apigateway.LambdaIntegration(
+        chatai_integration = LambdaIntegration(
             self.chatai_lambda,
             request_templates={"application/json": '{ "statusCode": "200" }'})
 
-        api.root.add_method("GET", chatai_integration)
+        # Add a method to the API Gateway with API key requirement
+        api.root.add_method("GET", chatai_integration, api_key_required=True)
+
+        # Create an API key
+        test_key = ApiKey(self, "DEV Test Key", api_key_name="devTestKey", enabled=True)
+
+        # Create a usage plan
+        development_usage_plan = UsagePlan(self,
+                                           "Dev Usage Plan",
+                                           api_stages=[
+                                               UsagePlanProps(api=api, stage=api.deployment_stage)
+                                           ],
+                                           throttle=ThrottleSettings(
+                                               rate_limit=100 / 3600,  # requests per second
+                                               burst_limit=10  # maximum number of requests in a burst
+                                           ),
+                                           quota={
+                                               "limit": 100,  # number of requests
+                                               "period": apigateway.Period.HOUR  # time period
+                                           }
+                                           )
+
+        # Associate the API key with the usage plan
+        development_usage_plan.add_api_key(test_key)
